90d5715962c531917ae6d8c385873b667c4c12d8
Cassini <mariocampos1175@gmail.com>
2024-08-27 13:47:07 -0600

    First commit

---
+++ src/main/java/com/bluered/App.java
@@ -0,0 +1,13 @@
+package com.bluered;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
